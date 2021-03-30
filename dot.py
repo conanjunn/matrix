b8351df269114ed19543b2840d41f841405e5b06
xuyang1 <xuyang1@huya.com>
2021-03-30 08:11:34 +0000

    ok

---
+++ dot.py
@@ -0,0 +1,76 @@
+# %%
+import numpy as np
+from matplotlib.patches import Polygon
+import matplotlib.pyplot as plt
+
+# %%
+v = np.array([1, 2, 3, 4, 5])
+v
+
+# %%
+m = np.array([[1, 2, 3, 4, 5], [1, 2, 3, 4, 5]])
+m
+
+# %%
+# 矩阵点乘向量将得到一个向量。可以将矩阵看做是原向量的一个函数
+m.dot(v)
+
+# %%
+m1 = np.array([[1, 1], [2, 2]])
+m1
+
+# %%
+m2 = np.array([[3, 3], [4, 4]])
+m2.T
+
+# %%
+m1.dot(m2)
+
+# %%
+m3 = np.array([[1, 1], [2, 2], [3, 3]])
+m3
+
+# %%
+m3.shape
+
+# %%
+m4 = np.array([[1, 1, 1], [2, 2, 2]])
+m4.shape
+
+# %%
+m3.dot(m4)
+
+# %%
+# 三角形
+triangle = np.array([[0, 0, 1], [3, 3, 1], [3, 0, 1]])
+triangle
+
+# %%
+triangle[:, :2]
+
+# %%
+
+
+def render(m, color):
+    fig, ax = plt.subplots()
+    ax.add_patch(Polygon(m, color=color))
+
+    ax.set_xlim(0, 5)
+    ax.set_ylim(0, 5)
+    plt.show()
+
+
+# %%
+render(triangle[:, :2], 'r')
+
+# %%
+# 矩阵平移
+a = 1
+b = 1
+m = triangle.dot(np.array([[1, 0, 0], [0, 1, 0], [a, b, 1]]))
+m
+
+# %%
+render(m[:, :2], 'b')
+
+# %%
